01d7ffec878762f7c1b7dfdbd170dffd8f3383b0
linfeng <cenlinfeng@hotmail.com>
2023-10-16 13:12:37 +0800

    增加按钮和选择文件夹标签

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_select_dir = new System.Windows.Forms.Button();
+            this.label_select_folder = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn_select_dir
+            // 
+            this.btn_select_dir.Location = new System.Drawing.Point(12, 12);
+            this.btn_select_dir.Name = "btn_select_dir";
+            this.btn_select_dir.Size = new System.Drawing.Size(75, 23);
+            this.btn_select_dir.TabIndex = 0;
+            this.btn_select_dir.Text = "选择文件夹";
+            this.btn_select_dir.UseVisualStyleBackColor = true;
+            this.btn_select_dir.Click += new System.EventHandler(this.btn_select_dir_Click);
+            // 
+            // label_select_folder
+            // 
+            this.label_select_folder.AutoSize = true;
+            this.label_select_folder.Location = new System.Drawing.Point(103, 17);
+            this.label_select_folder.Name = "label_select_folder";
+            this.label_select_folder.Size = new System.Drawing.Size(77, 12);
+            this.label_select_folder.TabIndex = 1;
+            this.label_select_folder.Text = "请选择文件夹";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_select_folder);
+            this.Controls.Add(this.btn_select_dir);
             this.Name = "Form1";
             this.Text = "修改文件名和关键词";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_select_dir;
+        private System.Windows.Forms.Label label_select_folder;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,40 @@ namespace ReplaceFileKeyWord
 {
     public partial class Form1 : Form
     {
+        private string folderPath = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_select_dir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK) 
+            {
+                folderPath = folderBrowserDialog.SelectedPath;
+                selectFolderComplete();
+            }
+        }
+
+        private void  selectFolderComplete() 
+        {
+            updateLabelSelectFolderText();
+
+        }
+
+
+
+        private void updateLabelSelectFolderText(string text = "") 
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                label_select_folder.Text = folderPath;
+            }
+            else 
+            {
+                label_select_folder.Text = text;
+            }
         }
     }
 }
