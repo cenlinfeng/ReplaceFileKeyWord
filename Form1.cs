c49896afa0a788fa8e9de2182a73985374f272ba
linfeng <cenlinfeng@hotmail.com>
2023-10-17 13:12:59 +0800

    增加两句代码

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,15 @@ namespace ReplaceFileKeyWord
     public partial class Form1 : Form
     {
         private string folderPath = "";
+        private SortedDictionary<string, string> dirction = new SortedDictionary<string, string>();
+
         public Form1()
         {
             InitializeComponent();
             initListView();
+            gridViewEnable(false);
         }
+
         private void initListView() 
         {
             DataGridViewTextBoxColumn oldNameColumn = new DataGridViewTextBoxColumn();
@@ -46,7 +51,7 @@ namespace ReplaceFileKeyWord
         private void  selectFolderComplete() 
         {
             updateLabelSelectFolderText();
-
+            gridViewEnable(true);
         }
 
 
@@ -65,12 +70,34 @@ namespace ReplaceFileKeyWord
 
         private void btn_replace_Click(object sender, EventArgs e)
         {
+            getUpdateKeyWord();//获取关键词
+            //在选择的文件夹里面找文件和文件夹
+            if (!File.Exists(folderPath)) {
+                MessageBox.Show("没有这个文件夹");
+                return;
+            }
+        }
+
+        private void getUpdateKeyWord() {
+
+            if (dirction.Count > 0)
+            {
+                dirction.Clear();
+            }
             //先遍历里面的数据
-            for (int i =0;i<data_grid_view.Rows.Count-1;i++) {
+            for (int i = 0; i < data_grid_view.Rows.Count - 1; i++)
+            {
                 string oldName = data_grid_view.Rows[i].Cells[0].Value.ToString();
                 string newName = data_grid_view.Rows[i].Cells[1].Value.ToString();
-                Console.WriteLine(oldName + " " + newName);
+                dirction.Add(oldName, newName);
             }
+        }
+
+
+        private void gridViewEnable(bool b = false) 
+        {
+            data_grid_view.Enabled = b;
+            btn_replace.Enabled = b;
         }
     }
 }
